ff35e0047f9dd4de2f314950dc73ccaafd034f9f
julioserafim <serafimjuliom583@gmail.com>
2016-11-16 17:50:05 -0300

    Criando Repositório

---
+++ src/main/java/com/projeto/ufc/MeuGarcomApplication.java
@@ -0,0 +1,12 @@
+package com.projeto.ufc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuGarcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuGarcomApplication.class, args);
+	}
+}
