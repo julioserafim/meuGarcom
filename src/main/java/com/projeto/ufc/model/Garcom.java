c68e5260781c230784812917e7709a78e3889626
julioserafim <serafimjuliom583@gmail.com>
2016-11-18 21:14:45 -0300

    "Alterando Garçom"

--- src/main/java/com/projeto/ufc/model/Garcom.java
+++ src/main/java/com/projeto/ufc/model/Garcom.java
@@ -1,5 +1,44 @@
 package com.projeto.ufc.model;
 
 public class Garcom {
+	private String nome;
+	private String sobrenome;
+	private int idade;
+	private String cpf;
+	
+	public Garcom(String nome, String sobrenome, int idade, String cpf) {
+		super();
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.idade = idade;
+		this.cpf = cpf;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+	
 
 }
