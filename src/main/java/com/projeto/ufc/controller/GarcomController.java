a1c2d1f4641d8a65ddb7fb7b738bc1c341ecb966
julioserafim <serafimjuliom583@gmail.com>
2016-11-21 21:49:06 -0300

    "Back-End garçom feito e configurações para acessar banco"

--- src/main/java/com/projeto/ufc/controller/GarcomController.java
+++ src/main/java/com/projeto/ufc/controller/GarcomController.java
@@ -1,5 +1,55 @@
 package com.projeto.ufc.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.projeto.ufc.dao.GarcomDao;
+import com.projeto.ufc.model.Garcom;
+
+@Transactional
+@Controller
 public class GarcomController {
+	
+	@Autowired
+	private GarcomDao garcomDao;
+	
+	@RequestMapping("/inserirGarcomFormulario")
+	public String inserirGarcomFormulario(){
+		return "página de cadastro garçom";
+	}
+	
+	@RequestMapping("/inserirGarcom")
+	public ModelAndView inserirGarcom(@Valid Garcom garcom, BindingResult result){
+		
+		ModelAndView mv = new ModelAndView("página de cadastro garçom");
+		garcomDao.save(garcom);
+		mv.addObject("mensagem", "Garçom cadastrado com sucesso");
+		return mv;
+	}
+	
+	@RequestMapping("/apagarGarcom")
+	public ModelAndView apagarGarcom(@RequestParam(value = "garcom") Long codigoGarcom){
+		ModelAndView mv = new ModelAndView("GarcomApagadoSucesso");
+		garcomDao.delete(codigoGarcom);
+		return mv;
+	}
+	
+	@RequestMapping("/listarGarcons")
+	public ModelAndView listarGarcons(@Valid Garcom garcom, BindingResult result){
+		List<Garcom> todosGarcons = garcomDao.findAll();
+		ModelAndView mv = new ModelAndView("ListarGarcons");
+		mv.addObject("garcos", todosGarcons);
+		return mv;
+		
+	}
 	
 }
--- src/main/java/com/projeto/ufc/model/Garcom.java
+++ src/main/java/com/projeto/ufc/model/Garcom.java
@@ -1,6 +1,12 @@
 package com.projeto.ufc.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Garcom {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	private String nome;
 	private String sobrenome;
@@ -38,6 +44,52 @@ public class Garcom {
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		result = prime * result + idade;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((sobrenome == null) ? 0 : sobrenome.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Garcom other = (Garcom) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		if (idade != other.idade)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (sobrenome == null) {
+			if (other.sobrenome != null)
+				return false;
+		} else if (!sobrenome.equals(other.sobrenome))
+			return false;
+		return true;
+	}
+	
+	
 	
 	
 	
